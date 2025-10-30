83a1bbf941f2b6ec9fec978ee0bea33c78d5836f
minwoo Jeong <46260961+hayannoon@users.noreply.github.com>
2025-10-30 23:57:30 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251030/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <climits>
+#include <cstdlib>
+using namespace std;
+int N;
+vector<pair<int,int>> v;
+int res = INT_MIN;
+
+int calcLength() {
+    int sum = 0;
+    for(int i = 1 ; i < N ; i++) {
+        sum += (abs(v[i].first-v[i-1].first) + abs(v[i].second - v[i-1].second));
+    }
+    return sum;
+}
+
+int main() {
+    cin >> N;
+    v.resize(N);
+    for(int i = 0 ; i < N ; i++) {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    for(int i = 1 ; i < N-1 ; i++) {
+        auto [x,y] = v[i];
+        v[i] = v[i-1];
+        res = max(res, calcLength());
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
