442f70cff4d9f8d6b74bdf1df20ce95d0cf28ae0
minwoo Jeong <46260961+hayannoon@users.noreply.github.com>
2025-10-30 23:24:50 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251030/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+string s;
+
+int main() {
+    cin >> s;
+
+    long long value = 0;
+    bool changeFlag = false;
+
+    for(char ch : s) {
+        if(ch != '0' && ch != '1') {
+            cerr << "this is not binary number\n";
+            return 1;
+        }
+        if(ch == '0' && !changeFlag) {
+            ch = '1';
+            changeFlag = true;
+        }
+        value = value * 2 + (ch - '0');
+    }
+
+    cout << value << "\n";
+
+    return 0;
+}
