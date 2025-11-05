d122da18a838bea9839e53ff05da2394eb8f97d9
minwoo Jeong <46260961+hayannoon@users.noreply.github.com>
2025-11-05 22:49:05 +0900

    Title: Carry 피하기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251105/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <climits>
+using namespace std;
+
+int N;
+
+int getSumIfCarry(int a, int b, int c) {
+    int backup = a+b+c;
+
+    while(a!=0 && b!=0 && c!=0) {
+        int sum = 0;
+        sum += a%10;
+        sum += b%10;
+        sum += c%10;
+
+        if(sum>10) return INT_MIN;
+
+        a /= 10;
+        b /= 10;
+        c /= 10;
+    }
+
+    return backup;
+}
+
+int main() {
+    cin >> N;
+    vector<int> v;
+    v.resize(N);
+    int res = INT_MIN;
+
+
+    for(int i = 0 ; i < N ; i++) {
+        cin >> v[i];
+    }
+
+    for(int i = 0 ; i < N-2 ; i++) {
+        for(int j = i+1 ; j < N-1 ; j++) {
+            for(int k = j+1 ; k < N ; k++) {
+                res = max(res, getSumIfCarry(v[i],v[j],v[k]));
+            }
+        }
+    }
+
+    cout << res << "\n";
+
+    return 0;
+}
