67916c01ae09922117260370f70ced496e963483
minwoo Jeong <46260961+hayannoon@users.noreply.github.com>
2026-02-21 22:28:36 +0900

    Title: 다양하게 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260221/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "Total days in Year\n\
+365\n\
+Circumference rate\n\
+3.1415926535";
+    return 0;
+}
